9da42e854079a0237fd193dac5372ffbd4b55ad2
Alexander Vinnik <VinnikAlex0@gmail.com>
2021-11-13 13:21:39 +1100

    updated code - fully functioning app

--- ScissorsPaperRock/app.js
+++ ScissorsPaperRock/app.js
@@ -2,6 +2,19 @@ const SELECTION = ["rock", "paper", "scissors"];
 
 const pick = document.querySelectorAll("button");
 
+let whoWon = document.querySelector(".display");
+
+let winner = "";
+
+let playerScore = document.querySelector(".player-score");
+let computerScore = document.querySelector(".computer-score");
+
+let playerCount = 0;
+let computerCount = 0;
+
+updateScore();
+updateWinner();
+
 // loop through each button element
 pick.forEach((button) => {
   // listen for users choice
@@ -30,33 +43,69 @@ function randomSelection() {
 function determineWinner(playerValue, computerValue) {
   // Checking for a tie
   if (playerValue === computerValue) {
+    winner = "Draw";
+    updateWinner();
     return "DRAW!";
   }
 
   // Checking for rock
   if (playerValue === "rock") {
     if (computerValue === "scissors") {
+      winner = "You Win";
+      updateWinner();
+      playerCount++;
+      updateScore();
       return "You Win!";
     } else {
+      winner = "You Lose";
+      updateWinner();
+      computerCount++;
+      updateScore();
       return "Computer Wins";
     }
   }
   // Checking for scissors
   if (playerValue === "scissors") {
     if (computerValue === "paper") {
+      winner = "You Win";
+      updateWinner();
+      playerCount++;
+      updateScore();
       return "You Win!";
     } else {
+      winner = "You Lose";
+      updateWinner();
+      computerCount++;
+      updateScore();
       return "Computer Wins";
     }
   }
   // Checking for paper
   if (playerValue === "paper") {
     if (computerValue === "rock") {
+      winner = "You Win";
+      updateWinner();
+
+      playerCount++;
+      updateScore();
       return "You Win!";
     } else {
+      winner = "You Lose";
+      updateWinner();
+      computerCount++;
+      updateScore();
       return "Computer Wins";
     }
   }
+}
+
+function updateScore() {
+  playerScore.innerHTML = playerCount;
+  computerScore.innerHTML = computerCount;
+}
+
+function updateWinner() {
+  whoWon.innerHTML = winner;
 }
 
 // console.log(determineWinner(playerPick(), randomSelection()));

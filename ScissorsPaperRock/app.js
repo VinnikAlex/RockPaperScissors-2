446f8630ef0c415a17af6e86363003b8fee2c3b4
Alexander Vinnik <VinnikAlex0@gmail.com>
2021-11-13 12:45:16 +1100

    updated js/html files

--- ScissorsPaperRock/app.js
+++ ScissorsPaperRock/app.js
@@ -1,20 +1,31 @@
 const SELECTION = ["rock", "paper", "scissors"];
 
+const pick = document.querySelectorAll("button");
+
+// loop through each button element
+pick.forEach((button) => {
+  // listen for users choice
+  button.addEventListener("click", () => {
+    // passing appropriate id into playerPick() function
+    const clicked = button.id;
+    //storing player and comp choices into variables
+    const playValue = clicked;
+    const compValue = randomSelection();
+
+    console.log(playValue);
+    console.log(compValue);
+
+    // passing player and computer choice variables to check who wins
+    console.log(determineWinner(playValue, compValue));
+  });
+});
+
 // returns random SELECTION
 function randomSelection() {
   const computerNumber = Math.floor(Math.random() * SELECTION.length);
   const computerChoice = SELECTION[computerNumber];
   return computerChoice;
 }
-// returns players selection
-function playerPick() {
-  let playerChoice = "rock";
-  return playerChoice;
-}
-//storing players choice
-const playValue = playerPick();
-//storing computers choice
-const computerValue = randomSelection();
 
 function determineWinner(playerValue, computerValue) {
   // Checking for a tie
@@ -23,7 +34,7 @@ function determineWinner(playerValue, computerValue) {
   }
 
   // Checking for rock
-  if (playValue === "rock") {
+  if (playerValue === "rock") {
     if (computerValue === "scissors") {
       return "You Win!";
     } else {
@@ -31,15 +42,15 @@ function determineWinner(playerValue, computerValue) {
     }
   }
   // Checking for scissors
-  if (playValue === "scissors") {
-    if (computerValue === "Paper") {
+  if (playerValue === "scissors") {
+    if (computerValue === "paper") {
       return "You Win!";
     } else {
       return "Computer Wins";
     }
   }
   // Checking for paper
-  if (playValue === "paper") {
+  if (playerValue === "paper") {
     if (computerValue === "rock") {
       return "You Win!";
     } else {
@@ -47,18 +58,22 @@ function determineWinner(playerValue, computerValue) {
     }
   }
 }
-// use all functions inside of game() function
-function game() {
-  playerPick();
-  randomSelection();
-  console.log(determineWinner(playerPick(), randomSelection()));
-}
 
-// loop through game() to play 5 rounds
-function playRound() {
-  for (let i = 0; i < 5; i++) {
-    game();
-  }
-}
-// use the function!
-playRound();
+// console.log(determineWinner(playerPick(), randomSelection()));
+// console.log(playerPick());
+
+// // use all functions prior functions
+// function game() {
+//   playerPick();
+//   randomSelection();
+//   console.log(determineWinner(playerPick(), randomSelection()));
+// }
+
+// // loop through game() to play 5 rounds
+// function playRound() {
+//   for (let i = 0; i < 5; i++) {
+//     game();
+//   }
+// }
+// // use the function!
+// playRound();
